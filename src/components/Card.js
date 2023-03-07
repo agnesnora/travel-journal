f0b3824b59f4e268cf89f7a93bce8f1255f56713
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-03-07 14:15:46 +0100

    css styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,20 @@
+import location from "../location.png";
+
 export default function Card(props) {
   return (
-    <div>
+    <div className="container">
       <img className="travel--img" src={props.item.imageUrl} alt="pthoto"></img>
-      <div>
-        <div>
-          <h2 className="travel--location">{props.item.location}</h2>
+      <div className="container--right">
+        <div className="travel--location">
+          <img src={location} alt="location-sign" />
+          <h2 className="travel--location-country">{props.item.location}</h2>
           <a href={props.item.googleMapsUrl}>View on google maps</a>
         </div>
         <h1 className="travel--title">{props.item.title}</h1>
-        <h3 className="travel--date">
+        <h2 className="travel--date">
           {props.item.startDate}-{props.item.endDate}
-        </h3>
-        <p>{props.item.description}</p>
+        </h2>
+        <p className="travel--description">{props.item.description}</p>
       </div>
     </div>
   );
